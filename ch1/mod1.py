6122474dd08df457ecfc6c151c6a439527c712b0
sanghunyun <godduk7772@gmail.com>
2026-07-23 17:51:33 +0900

    7월23일

---
+++ ch1/mod1.py
@@ -0,0 +1,33 @@
+# #calc.py 모듈 사용
+# import calc
+
+
+# print(calc.add(7,5))
+# print(calc.sub(7,5))
+
+# from calc import add, sub, Math
+# print(add(7,5))
+# print(sub(7,5))
+
+# math = Math()
+
+# print(math.solv(5))
+
+
+# from calc import *
+# math = Math()
+# print(math.solv(5))
+# print(add(7,5))
+# print(sub(7,5))
+
+
+from calc improt*
+from mod2 improt*
+from mod3 improt*
+
+math = Math()
+
+print(math.solv(5))
+print(add(7,5))
+print(sub(7,5))
+max()
